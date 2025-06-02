b48498036a0e8ab55bd2636de0e458347b7d0263
bagrov vv <bagrov-v@yandex.ru>
2025-06-02 18:52:28 +0300

    cmt aPlusB4

--- hw1/main.go
+++ hw1/main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter text")
-	s, err := reader.ReadString('\n')
-	// s, _, err := reader.ReadRune()
-	if err != nil {
-		fmt.Println(err)
-	}
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Println("Enter text")
+	// s, err := reader.ReadString('\n')
+	// // s, _, err := reader.ReadRune()
+	// if err != nil {
+	// 	fmt.Println(err)
 
-	fmt.Println(string(s))
-	fmt.Printf("dfsdfdsf %s", string(s))
+	// fmt.Println(string(s))
+	// fmt.Printf("dfsdfdsf %s", string(s))
 	fmt.Printf("edewdwed")
+
+	APlusB()
 }
 
-func AplusB() int {
-	return 5 + 5
+func APlusB() int {
+	return 4 + 4
 }
